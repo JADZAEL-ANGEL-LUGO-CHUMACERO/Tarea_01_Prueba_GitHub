94ba47035bd872e6b83d5d5d35d9cc62d94c1b1e
JADZAEL-ANGEL-LUGO-CHUMACERO <79479158+JADZAEL-ANGEL-LUGO-CHUMACERO@users.noreply.github.com>
2021-02-23 16:07:20 -0600

    Agregando programas

---
+++ HolaMundo_Jadzael Angel Lugo Chumacero.c.c
@@ -0,0 +1,12 @@
+//biblioteca principal
+#include<stdio.h>
+
+//para la funcion principal 
+int main(){
+	
+	printf(" Hola Mundo\n ");
+	printf("Jadzael Angel Lugo Chumacero ");
+	
+	
+	return 0;
+}
